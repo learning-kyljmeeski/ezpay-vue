5ec12f7bd3af571cc3b827bd0ac591c892bf5a6c
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-31 19:09:06 +0600

    add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <v-app>
-    <Header />
-    <v-main>
-      <!-- <HelloWorld /> -->
-      <Integration />
-    </v-main>
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Главная</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>О нас</v-list-item-title>
+        </v-list-item>
+        <!-- Добавьте другие пункты меню, если необходимо -->
+      </v-list>
+    </v-navigation-drawer>
 
+    <v-main>
+      <router-view />
+    </v-main>
     <AppFooter />
   </v-app>
 </template>
 
 <script setup>
-import Integration from './components/Integration.vue';
-import Header from "@/components/Header.vue";
 
-  //
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,6 @@
     <v-app-bar-title style="cursor: pointer" @click="$router.push('/')">Home</v-app-bar-title>
     <v-spacer></v-spacer>
       <v-btn color="purple-accent-3" to="/home">Home</v-btn>
-      <v-btn color="purple-accent-3" icon="mdi-logout" size="small" @click="logout"/>
+      <v-btn color="purple-accent-3" icon="mdi-logout" size="small" @click="() => {}"/>
   </v-app-bar>
 </template>
