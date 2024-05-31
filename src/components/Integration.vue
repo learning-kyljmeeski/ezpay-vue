22df3bb82f11f0ad688a21e8b5e9e3e91ed28358
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-31 18:44:03 +0600

    int

--- src/components/Integration.vue
+++ src/components/Integration.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="text-bg-primary">
-        Integration
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../assets/integration-image.png" alt="image" class="img-fluid ml-n1">
+            </div>
+            <div class="col-md-6">
+                <h2 class="fs-5 text fw-bold mb-4">
+                    Полностью интегрированный набор платежных продуктов
+                </h2>
+                <p class="fw-lighter fs-6 text">
+                    Мы объединяем все, что требуется для создания веб-сайтов и приложений, которые принимают платежи и
+                    отправляют выплаты по всему миру. Продукты Stripe позволяют осуществлять платежи онлайн и лично
+                    розничным продавцам, компаниям, занимающимся подпиской, программным платформам и маркетплейсам, а
+                    также всему, что находится между ними.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
