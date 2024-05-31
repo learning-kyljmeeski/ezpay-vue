d452266e0b1d58b1a1a2ac02a42591ef38c6bb39
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2024-06-01 05:33:18 +0600

    add all sections to landing page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-app>
-    <SideBar />
-    <TopBar/>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <v-layout>
+    <v-app>
+      <SideBar/>
+      <TopBar/>
+      <v-main>
+        <router-view/>
+      </v-main>
+      <v-footer class="bg-transparent container mt-4">
+
+        <div class="me-auto">
+          <p>© 2024 Copyright, All Right Reserved, Made by Ilya Yugai with</p>
+        </div>
+
+        <div class="ms-auto">
+          <img src="./assets/social.png" alt="social media" class="img-fluid">
+        </div>
+      </v-footer>
+    </v-app>
+  </v-layout>
 </template>
 
 <script setup>
